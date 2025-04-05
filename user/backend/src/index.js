22aaceef0ad6f9d895a460bd21d569913daa5699
Mayank Raj <mayank230106raj@gmail.com>
2025-04-05 08:14:56 +0530

    user backend api issue sorted

--- user/backend/src/index.js
+++ user/backend/src/index.js
@@ -1,3 +1,22 @@
-console.log("Server starting...");
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import connectDB from './src/config/db.js';
 import purchaseRoutes from './routes/purchaseRoutes.js';
+
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 app.use('/api/purchase', purchaseRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
